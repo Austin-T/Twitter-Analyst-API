138e7884f369b7c0ccc135ad14ae60ffec6f7a37
Austin Tralnberg <atralnberg@gmail.com>
2020-07-11 19:07:25 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from secretKeys import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({"about": "Hello World"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
